5da11984d6c4c48d72ece040c16325a8f9a5dd84
roman.babukh <roman.babukh@rsrg.com>
2023-11-26 11:59:15 +0100

    Dynamic options

--- assets/js/dataFunctions.js
+++ assets/js/dataFunctions.js
@@ -1,14 +1,27 @@
 const localeKey = 'l';
-const defaultLocale = 'en'
+const defaultLocale = 'uk'
+const defaultSupportedLangs = {
+    'uk': 'українська',
+    'en': 'англійська',
+    'de': 'німецька'
+}
 
 const getLocale = () => {
     const locale = localStorage.getItem(localeKey)
+    const langs = getSupportedLanguages()
+
     if (!locale) {
         setLocale(defaultLocale)
         return defaultLocale
     }
-
-    return locale
+    for (const [key, value] of Object.entries(langs)) {
+        if(locale === key){
+            return locale
+        }
+    }
+    console.log(langs)
+    setLocale(defaultLocale)
+    return defaultLocale
 }
 
 const setLocale = (l) => {
@@ -47,7 +60,7 @@ const fillWorks = (data) => {
         $.each(work.entries, function (i, entry) {
             html += `<li>${entry}</li>`
         })
-        
+
         html += '</ul></p></p>'
 
         worksHtml += html
@@ -57,22 +70,55 @@ const fillWorks = (data) => {
 
     const finalHtml = worksTitleHtml + worksHtml
 
-    
-
     $('#workSection').html(finalHtml)
     // <p>Afwerking werkt<p><ul class="b"><li>Tegel werkt</li><li>Schilderen werkt</li><li>Gips werkt</li></ul></p></p>
 }
 
+const getSupportedLanguages = () => {
+    if (localStorage.getItem('supportedLanguages')) {
+        return JSON.parse(localStorage.getItem('supportedLanguages'))
+    }
+
+    $.get("../../readme.txt", function (data) {
+        const lines = data.split('\n');
+        const languages = {};
+
+        lines.forEach((line) => {
+            const [code, name] = line.split(/\s+/);
+            if (code && name) {
+                languages[code] = name;
+            }
+        });
+
+        localStorage.setItem('supportedLanguages', JSON.stringify(languages))
+    }, 'text');
+
+    return defaultSupportedLangs
+}
+
+const fillLanguages = () => {
+    const langs = getSupportedLanguages()
+
+    let finalHtml = '<option value="ua" class="t-chooseLanguage"></option>'
+    for (const [key, value] of Object.entries(langs)) {
+        finalHtml += `<option value="${key}">${value}</option>`
+    }
+
+    $('#languageSelect').html(finalHtml)
+}
+
 $(document).ready(function () {
+    fillLanguages()
+
     $.getJSON(getDataFile(), function (data) {
         fillStaticText(data)
 
         fillWorks(data)
     });
 
-    $('#languageSelect').change(function() {
+    $('#languageSelect').change(function () {
         var selectedLanguage = $(this).val();
         setLocale(selectedLanguage)
         location.reload();
-      });
+    });
 });
